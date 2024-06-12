4a6bcf5239525828530b5e354eeee3ebc6822671
Maybaba <sinyunjong@gmail.com>
2024-06-12 10:47:19 +0900

    # 동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from "react";
-import Hello from "./components/Hello.js"
-import Bye from "./components/Bye.js"
+import ExpenseItem from "./components/expenses/ExpenseItem.js"
 
-function App() {
+
+const App = () => {
+    return (
+        <div>
+            <ExpenseItem />
+            
+        </div>
+    );
+};
+
+export default App;
+
+// function App() {
 
     //jsx 문법
     // const $h2 = React.createElement('h2', null, '방가방가방가방가방가방가');
-    const $h2 = <h2>방가방가</h2>;
+    // const $h2 = <h2>방가방가</h2>;
 
     //jsx 규칙
     //1. return 안에 있는 태그는 반드시 하나의 태그로 묶여야 함 : 의미없는 부모 div 클래스를 만드는 대신 5번을 한다.
@@ -17,17 +28,17 @@ function App() {
     //4. js의 for 문으로 인식해서 label은 꼭 htmlFor로 작성해야 한다.
     //5. 의미없는 부모는 <React.Fragment> 로 감싼다. 여기서 더 축약해서 <> 다이아몬드로 표기 가능하다.
     //6. 변수 값이나 함수를 출력할 때는 {}로 감싸면 된다.
-    const hello = "하이";
-  return (
-      <>
-          <Hello />
-          <Bye />
-          <Hello />
-          <Hello />
-          <Hello />
-          </>
-
-  );
-}
-
-export default App;
+//     const hello = "하이";
+//   return (
+//       <>
+//           <Hello />
+//           <Bye />
+//           <Hello />
+//           <Hello />
+//           <Hello />
+//           </>
+//
+//   );
+// }
+//
+// export default App;
