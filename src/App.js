d67e2c4f0df3a8a01f9bf87d40452a78a3b5e70f
Maybaba <sinyunjong@gmail.com>
2024-06-13 12:41:26 +0900

    # 동적 렌더링

--- src/App.js
+++ src/App.js
@@ -10,19 +10,24 @@ const App = () => {
     // 서버에서 지출항목 JSON 배열을 응답받음
     const expenses = [
         {
-            title: '책 구매',
-            price: 30000,
-            date: new Date(2024, 6-1, 3) // 1월이 0으로 되어있고 2월이 1이 되어있음
+            title: '전공책',
+            price: 10000,
+            date: new Date(2024, 2-1, 3) // 1월이 0으로 되어있고 2월이 1이 되어있음
         },
         {
-            title: '족발먹음',
-            price: 40000,
-            date: new Date(2024, 6-1, 7)
+            title: '손금책',
+            price: 2000,
+            date: new Date(2023, 7-1, 7)
         },
         {
-            title: '헬스장등록',
-            price: 300000,
-            date: new Date(2024, 6-1, 12)
+            title: '아이스크림케이크',
+            price: 44000,
+            date: new Date(2021, 2-1, 29)
+        },
+        {
+            title: '그릭요거트',
+            price: 3000,
+            date: new Date(2022, 13-1, 11)
         },
     ];
 
--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -1,36 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseItem from './ExpenseItem.js'
 import ExpenseFilter from './ExpenseFilter.js'
 
 const ExpenseList = ({ expenses }) => {
 
-const onFilterChange = (v) => {
+  //선택된 연도로 재 렌더링 하기 위해 연도를 상태값으로 관리
+  const [filteredYear, setFilteredYear] 
+  = useState(new Date().getFullYear()) ;
+
+  const onFilterChange = (v) => {
   //ExpenseFilter에 있는 선택된 연도값을 여기서 출력하기
   console.log('선택된 년도 출력 :', v);
+  setFilteredYear(filteredYear);
 }
 
-  return (
-    <div className='expenses'>
+//app에서 받은 expenses 배열을 <ExpenseItem> 배열로 변환하는 함수 만들기
+// const convertToComponentArray = () => {
 
-      <ExpenseFilter onFilterChange={onFilterChange} />
+//   return expenses
+//   .map(ex => <ExpenseItem title={ex.title} price={ex.price} date={ex.date} />);
 
-            <ExpenseItem
-                title={expenses[0].title}
-                price={expenses[0].price}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                price={expenses[1].price}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                price={expenses[2].price}
-                date={expenses[2].date}
-            />
-    </div>
-  )
-}
+  // const newArray = [];
+  // for (const ex of expenses) {
+  //   const tag = <ExpenseItem title={ex.title} price={ex.price} date={ex.date} />;
+  //   newArray.push(tag);
+  // }
+  // return newArray;
+// }
 
-export default ExpenseList
+return (
+  <div className="expenses">
+    <ExpenseFilter onChangeFilter={onFilterChange} />
+
+    {expenses
+      .filter(ex => ex.date.getFullYear().toString() === filteredYear)
+      .map(({title, price, date}) => (
+      <ExpenseItem
+      //키를 보통은 PK 로 넣는다. 
+         key={Math.random().toString}// 아이디 같은 키 값을 부여한다. 실제 태그에는 그려지지 않는다. 여러개의 ex 아이템을 구분하기 위한 용도로 사용.
+        title={ex.title}
+        price={ex.price}
+        date={ex.date}
+      />
+    ))}
+  </div>
+);
+};
+
+export default ExpenseList;
