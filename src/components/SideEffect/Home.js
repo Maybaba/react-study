9f41f502cba8dec2a64fa911fa2b5b25c7b81517
Maybaba <sinyunjong@gmail.com>
2024-06-19 09:52:27 +0900

    # use context hook

--- src/components/SideEffect/Home.js
+++ src/components/SideEffect/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Card from '../UI/Card';
 import styles from './Home.module.css';
@@ -6,20 +6,15 @@ import Button from "../UI/Button";
 import AuthContext from "../../store/auth-context";
 
 const Home = () => {
-    return (
-    <AuthContext.Consumer>
-        {
-            (ctx) => {
-                console.log(ctx);
-                return(
-                    <Card className={styles.home}>
-                        <h1>Welcome back!</h1>
-                        <Button onClick={ctx.onLogout}> Logout </Button>
-                    </Card>
-                );
-            }
-        }
-    </AuthContext.Consumer>
+
+    const ctx= useContext(AuthContext);
+    console.log('ctx :',ctx);
+
+    return(
+        <Card className={styles.home}>
+            <h1>Welcome back!</h1>
+            <Button onClick={ctx.onLogout}> Logout </Button>
+        </Card>
     );
 };
 
--- src/components/SideEffect/Navigation.js
+++ src/components/SideEffect/Navigation.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from'./Navigation.module.css'
 import AuthContext from "../../store/auth-context";
 
 const Navigation = () => {
 
-    const loginPage =    (onLogout) => (
+    const loginPage = (onLogout) => (
         // 리액트에서 변수에 저장 할 때도 프래그먼트, 전체를 묶는 태그가 필요하다.
         <>
             <li>
@@ -21,22 +21,21 @@ const Navigation = () => {
         </>
     );
 
-      let anonymousPage = <li> <button>Sign up</button></li>
+    const anonymousPage = <li>
+        <button>Sign up</button>
+    </li>
+
+    const {isLoggedIn, onLogout} = useState(AuthContext);
 
     return (
-        <AuthContext.Consumer>
-            {({isLoggedIn, onLogout}) => {
-                // console.log(context);
-                return (
-                    <nav className={styles.nav}>
-                        <ul>
-                            {isLoggedIn ? loginPage(onLogout) : anonymousPage}
-                        </ul>
-                    </nav>
-                );
-            }}
-        </AuthContext.Consumer>
+        <nav className={styles.nav}>
+            <ul>
+                {isLoggedIn ? loginPage(onLogout) : anonymousPage}
+            </ul>
+        </nav>
     );
 };
+
+
 
 export default Navigation;
