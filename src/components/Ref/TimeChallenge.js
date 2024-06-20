3408216164f5da224e7fc4fcccc7d435bea698b1
Maybaba <sinyunjong@gmail.com>
2024-06-20 11:20:41 +0900

    # UseRef & portals practice 1. useref로 timer 공유 방지

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,6 +1,9 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 const TimerChallenge = ({title, targetTime}) => {
+
+    //timer를 ref 변수로 관리 " closure처럼 활용할 수 있음
+    const timer = useRef();
 
     //타이머가 시작되엇는지를 확인하는 상태값
     const [timerStarted, setTimerStarted] = useState(false);
@@ -8,11 +11,9 @@ const TimerChallenge = ({title, targetTime}) => {
     //타겟 시간이 종료되었는지 여부
     const [timerExpired, setTimerExpired] = useState(false);
 
-
-
     const startHandler = e => {
 
-        setTimeout(e => {
+        timer.current = setTimeout(e => {
             setTimerExpired(true);
             console.log('시간만료');
         }, targetTime*1000);
@@ -20,6 +21,10 @@ const TimerChallenge = ({title, targetTime}) => {
         setTimerStarted(true);
     }
 
+    const stopHandler = e => {
+        clearTimeout(timer.current);
+        console.log('stop!!!!!');
+    }
 
     return (
         <section className="challenge">
@@ -29,7 +34,7 @@ const TimerChallenge = ({title, targetTime}) => {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={startHandler}>
+                <button onClick={timerStarted ? stopHandler : startHandler}>
                     {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
