4909cdba87c36e81160e93dbbb92a4650a619dac
Maybaba <sinyunjong@gmail.com>
2024-06-20 12:34:02 +0900

    # UseRef & portals practice 1. useref로 timer 공유 방지 2. dialog 태그 제어 연습 3. forwardRef 로 모달 제어하기 4. setInterval 처리 5. 승패조건 처리

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -6,9 +6,11 @@ const isLost = remainingTime <= 0;
 
 const formattedRemainingTime = (remainingTime/1000).toFixed(2);
 
+const score = Math.round((1- remainingTime / (targetTime * 1000)) * 100);
+
     return (
         <dialog ref={ref} className="result-modal">
-            {isLost && <h2>Your lost!</h2>}
+            {isLost ? <h2>Your lost!</h2> : <h2>Yore Score : {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
             <form method="dialog">
