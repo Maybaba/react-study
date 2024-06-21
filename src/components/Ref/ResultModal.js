6bee129f03551e7933aca7b449d56e96670947e8
Maybaba <sinyunjong@gmail.com>
2024-06-21 09:05:37 +0900

    # changed resultmodal

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -10,7 +10,7 @@ const score = Math.round((1- remainingTime / (targetTime * 1000)) * 100);
 
     return (
         <dialog ref={ref} className="result-modal">
-            {isLost ? <h2>Your lost!</h2> : <h2>Yore Score : {score}</h2>}
+            {isLost ? <h2>Your lost!</h2> : <h2>Yore Samstcore : {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
             <form method="dialog">
