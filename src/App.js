6c986e0fa3ab0e98f845b1f0bf42f9261f81a564
Maybaba <sinyunjong@gmail.com>
2024-07-02 11:49:51 +0900

    # Router - 중첩 라우터 설정하기 - 라우터 상대경로 설정하기

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Home from './components/routeExample/pages/Home';
 import Products from './components/routeExample/pages/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from "./components/routeExample/layout/RootLayout";
+import ErrorPage from "./components/routeExample/pages/ErrorPage";
 
 // 라우터 설정
 const router = createBrowserRouter([
 //슬래스로 들어오면 엘리먼트를 루트레이아웃으로 로딩하겠다.
+
+    //절대경로로 path를 정할 수 있다.
+    //home을 링크를 비워두고, 하위 경로에서는 ./으로 경로를 장석하면 알아서 자동으로 화면을 로ㅎ딩한다.
     {
         path: '/',
         element: <RootLayout />,
+        errorElement: <ErrorPage />,
         children: [
-            { path: '/', element: <Home /> },
-            { path: '/products', element: <Products /> }
+            { index: true, element: <Home /> },
+            { path: 'products', element: <Products /> }
         ]
     },
 ]);
---
+++ src/components/routeExample/pages/ErrorPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import MainNavigation from "../layout/MainNavigation";
+
+const ErrorPage = () => {
+    return (
+        <>
+            <MainNavigation />
+        <main>
+            <h1>페이지를 찾을 수 없습니다 ! </h1>
+            <p>요청하신 페이지의 주소를 다시 확인해 주세요. </p>
+            <img src="/src/assets/img/noghtErrorpage.png" alt="errorpage"/>
+        </main>
+        </>
+    );
+};
+
+export default ErrorPage;
