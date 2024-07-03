77487129214152dbc7392957923a21d2763db44b
Maybaba <sinyunjong@gmail.com>
2024-07-03 10:45:56 +0900

    #useEffect 삭제 후 app.js에서 loader로 라우터 관리

--- src/components/routeExample/pages/Events.js
+++ src/components/routeExample/pages/Events.js
@@ -5,26 +5,15 @@ import EventsNavigation from '../layout/EventNavication';
 
 const Events = () => { 
 
-    //상태변수로 이벤트아이디를 반환해서 usestate 변수로 활용한다. 
-    const[eventList, setEventList] = useState([]);
 
-    //배열을 비워놓으면 최초 한번만 페치가 실행된다. 
-    useEffect(() => {
-         //렌더링 되기 전에 json을 요청한다. 
-    fetch('http://localhost:8282/events')
-    .then(res => res.json())
-    .then(jsonData => {
-        console.log(jsonData);
-        setEventList(jsonData); 
-    })
-    }, []);
+    //loader가 리턴한 데이터 받아오기, 훅을 사용하자
+    const aaa = useLoaderData();
+    console.log(aaa);
 
     return (
         <>
         <h1> Event Page </h1>
-        {/* < EventsNavigation /> */}
-        < EventList eventList={eventList} />
-     
+    
         </>
     );
 };
