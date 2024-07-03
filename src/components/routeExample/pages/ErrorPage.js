7cd93b5758cffd5786a333d41c455c75acda2970
Maybaba <sinyunjong@gmail.com>
2024-07-03 12:15:39 +0900

    #데이터 조회 제대로 수정하기 전

--- src/components/routeExample/pages/ErrorPage.js
+++ src/components/routeExample/pages/ErrorPage.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import MainNavigation from "../layout/MainNavigation";
+import { useRouteError } from 'react-router-dom';
+import EventsNavigation from '../layout/EventNavication';
 
 const ErrorPage = () => {
+
+    //에러가 발생했을 때 throw에 전달한 객체를 읽는 방법
+
+    //Events.js에서 던지는 메시지를 읽을 수 있다. 
+    const error = useRouteError();
+    // console.log("error from Events:",error.data);
+    let errorMessage; 
+    errorMessage = JSON.parse(error.data).message;
+
+    if (error.status == 400) {
+        errorMessage = JSON.parse(error.data).message;
+    }
+
+    if (error.status == 404) {
+        errorMessage = "페이지를 찾을 수 없습니다. URL을 확인하세요";
+    }
+    if (error.status == 500) {
+        errorMessage = "서버가 불안정합니다. 몇 분 후 다시 접속해 주세요.";
+    }
+
+
     return (
         <>
             <MainNavigation />
+            <EventsNavigation />
         <main>
             <h1>페이지를 찾을 수 없습니다 ! </h1>
-            <p>요청하신 페이지의 주소를 다시 확인해 주세요. </p>
+            <p> {errorMessage} </p>
             <img src="/src/assets/img/noghtErrorpage.png" alt="errorpage"/>
         </main>
         </>
--- src/components/routeExample/pages/Events.js
+++ src/components/routeExample/pages/Events.js
@@ -7,12 +7,13 @@ const Events = () => {
 
     // loader가 리턴한 데이터 받아오기
     const eventList = useLoaderData();
-    // console.log(eventList);
+    console.log(eventList);
 
+    console.log('event page rendering !');
     return (
         <>
             <h1>Events Page</h1>
-            <EventList eventList={eventList} />
+            <EventList eventList={ eventList } />
         </>
     );
 };
@@ -23,16 +24,28 @@ export default Events;
 
 // 이 페이지가 열릴때 자동으로 트리거되어 호출되는 함수
               // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
-            
-            //   export const loader = async () => fetch('http://localhost:8282/events');
-export const loader = async () => {
-        // console.log('loader call!!');
-        const response = await fetch('http://localhost:8282/events');
-        // const jsonData = await response.json();
 
-        // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서
-        // 언제든 불러서 사용할 수 있다.
-        // console.log(jsonData);
-        return response;
-        //loader 에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해준다. 
-};
+            // loader를 app.js로부터 아웃소싱
+            
+            // export const loader = async () => fetch('http://localhost:8282/events');
+            
+            export const loader = async () => {
+
+        
+            
+              console.log('loader call!');
+            
+              const response = await fetch('http://localhost:8282/events');
+
+        
+              if(!response.ok) {
+                const error = await response.error();
+                // throw new Error(); // 코드의 흐름을 끊어서 400시 데이터 연결하지 않기
+                throw new Response(JSON.stringify({ abc: error })), 
+                {
+                    status: response.status
+                };
+              };
+              return response; //200 일 경우 
+            };
+            
