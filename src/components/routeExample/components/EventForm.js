dddf199e05efcfcfe942c9e898fedc5a831695dc
Maybaba <sinyunjong@gmail.com>
2024-07-04 11:37:16 +0900

    # Cannot destructure property 'title' of 'event' as it is undefined.

--- src/components/routeExample/components/EventForm.js
+++ src/components/routeExample/components/EventForm.js
@@ -3,77 +3,136 @@ import React from 'react';
 import styles from './EventForm.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
 
-const EventForm = ({ method, event = {} }) => {
+  const EventForm = ({ method, event={} }) => {
 
-  const {
-    title,
-    desc: description,
-    'img-url': image,
-    'start-date': date
-  } = event;
-
-  // const { eventId: id } = useParams();
-  const navigate = useNavigate();
-
-  const cancelHandler = (e) => {
-    // window.location.href = '/events/' + id;
-    // navigate('/events/' + id);
-    navigate('..');
+    const {
+      title,
+      desc: description,
+      'imageUrl': image,
+      'start-date': date
+    } = event;
+  
+    // 날짜 형식을 변경 (yyyy-MM-dd)
+    /**
+     * 
+     * @param date - yyyy년 MM월 dd일
+     */
+    const convertDateFormat = (date) => {
+      const [yearPart, monthDayPart] = date.split('년 ');
+      const [monthPart, dayPart] = monthDayPart.split('월 ');
+  
+      const day = dayPart.replace('일', '');
+  
+      // console.log('date: ', { yearPart, monthPart, day });
+  
+      return `${yearPart}-${monthPart}-${day}`;
+    };
+  
+    let formatDate;
+    if(event.date) {
+    const formatDate = convertDateFormat(date);
   };
+  
+  
+    // const { eventId: id } = useParams();
+    const navigate = useNavigate();
+  
+    const cancelHandler = (e) => {
+      // window.location.href = '/events/' + id;
+      // navigate('/events/' + id);
+      navigate('..');
+    };
 
-  return (
-    <form className={styles.form}>
-      <p>
-        <label htmlFor="title">Title</label>
-        <input
-          id="title"
-          type="text"
-          name="title"
-          required
-          defaultValue={event ? title : ''}
-        />
-      </p>
-      <p>
-        <label htmlFor="image">Image</label>
-        <input
-          id="image"
-          type="url"
-          name="image"
-          required
-          defaultValue={event ? image : ''}
-        />
-      </p>
-      <p>
-        <label htmlFor="date">Date</label>
-        <input
-          id="date"
-          type="date"
-          name="date"
-          required
-          defaultValue={event ? date : ''}
-        />
-      </p>
-      <p>
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          name="description"
-          rows="5"
-          required
-          defaultValue={event ? description : ''}
-        />
-      </p>
-      <div className={styles.actions}>
-        <button
-          type="button"
-          onClick={cancelHandler}
-        >
-          Cancel
-        </button>
-        <button>{method === 'post' ? 'Save' : 'Modify'}</button>
-      </div>
-    </form>
-  );
-};
+    const submitHandler = e => {
+      //새로고침 꼭 방지하기 ! 
+      e.preventDefault();
+      console.log('form이 제출됨 ! ');
 
-export default EventForm;
+      // input에 입력한 값 가져오기 
+      //입력한 값 읽는 방법 3가지 
+      const formData = new FormData(e.target);
+      console.log('form: ', formData.get('title'));
+
+      //서버에 보낼 데이터
+      const payload = {
+        title: formData.get('title'),
+        desc: formData.get('descriction'),
+        imgUrl: formData.get('image'),
+        beginDate: formData.get('date') 
+      }
+
+      console.log('payload: ', payload);
+
+      //서버로 async 페칭하기
+    (async () => {
+      const response = await fetch(`http://localhost:8282/events`, {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(payload)
+      });
+      navigate('/events');
+      
+    })();
+    };
+
+
+  
+    return (
+      <form className={styles.form} onSubmit={submitHandler} noValidate>
+        <p>
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            type="text"
+            name="title"
+            required
+            defaultValue={event ? title : ''}
+          />
+        </p>
+        <p>
+          <label htmlFor="image">Image</label>
+          <input
+            id="image"
+            type="url"
+            name="image"
+            required
+            defaultValue={event ? image : ''}
+          />
+        </p>
+        <p>
+          <label htmlFor="date">Date</label>
+          <input
+            id="date"
+            type="date"
+            name="date"
+            required
+            defaultValue={event ? formatDate : ''}
+          />
+        </p>
+        <p>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            rows="5"
+            required
+            defaultValue={event ? description : ''}
+          />
+        </p>
+        <div className={styles.actions}>
+          <button
+            type="button"
+            onClick={cancelHandler}
+          >
+            Cancel
+          </button>
+          <button>{method === 'post' ? 'Save' : 'Modify'}</button>
+        </div>
+      </form>
+    );
+  };
+  
+  export default EventForm;
+  
