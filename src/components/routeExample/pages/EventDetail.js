7b1c2f8921fbf17932848466136f7e166a383c72
Maybaba <sinyunjong@gmail.com>
2024-07-04 10:01:01 +0900

    # event detail

--- src/components/routeExample/pages/EventDetail.js
+++ src/components/routeExample/pages/EventDetail.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {useLoaderData, useParams} from "react-router-dom";
+import EventItem from '../components/EventItem';
 
 const EventDetail = () => {
+
+    //라우터에서 주소에 붙은 값을 변환한다. 
     const { eventId: id } = useParams();
+    const [ ev, setEv ] = useState({});
   
-    const data = useLoaderData();
-    console.log('loader data: ', data);
+    // const data = useLoaderData();
+    // console.log('loader data: ', data);
+
+    //loader 말고 useEffect 가져오는 방법
+    useEffect(() => {
+
+        ( async () => {
+            const response = await fetch(`http://localhost:8282/events/${id}`);
+
+            //예외처리
+
+            const json = await response.json();
+            console.log("json : ",json);
+            setEv(json);
+        })();
+
+    }, []);
+
+    return <EventItem event={ev} />; 
+};
   
-    return (
-      <>
-        <h1>EventDetail Page</h1>
-        <p>Event ID: {id}</p>
-      </>
-    );
+    /*
+    return   <div>
+    {ev ? (
+        <>
+            <h1>EventDetail Page</h1>
+            <p>{ev.title}</p>
+            <p>{ev.desc}</p>
+            <p>{ev['start-date']}</p>
+        </>
+    ) : (
+        <p>Loading...</p>
+    )}
+</div>
   };
+  */
+ 
   
   export default EventDetail;
